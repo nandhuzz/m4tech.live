d1b28eac3cee0586dc0cfef0f28b70dbd861cc4c
Anandhu <anandhu7299@gmail.com>
2021-05-06 15:51:01 +0530

    development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Youtube from './Youtube/SubscribeCount'
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Youtube />
+
+    
     </div>
+    <h6 className="condition"> *displayed value is not realtime </h6>
+    <h2 className="footer">Development Mode</h2>
+</>
   );
 }
 
